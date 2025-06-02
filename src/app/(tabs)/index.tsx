629028a5921c10072dca4371b23c64f48eb21569
Mann919-dusk <manasseh.ameyow@glodusk.com>
2025-06-02 21:10:34 +0200

    mediaListItem flatlist

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,35 @@
-import { Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import mediaList from "@assets/data/mediaList.json";
 import MediaListItem from "@/components/MediaListItem";
 
 export default function HomeScreen() {
-
-
   return (
     <View>
-      <MediaListItem mediaItem={mediaList[1]}/>
+      <View>
+        <Text>For Mann</Text>
+      </View>
+      <FlatList
+        data={mediaList}
+        renderItem={({ item:veriticalListItem }) => (
+         <View>
+           <Text style={styles.sectionTitle}>{veriticalListItem.title}</Text>
+           <FlatList
+           horizontal
+           data={veriticalListItem.data}
+           renderItem={({item:horizontalListItem})=><MediaListItem mediaItem={horizontalListItem} />}
+           />
+         </View>
+        )}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  sectionTitle: {
+    fontSize: 17,
+    color: "white",
+    fontWeight: "700",
+    paddingVertical: 10,
+  },
+});
--- src/components/MediaListItem.tsx
+++ src/components/MediaListItem.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import mediaList from "@assets/data/mediaList.json";
-import { MediaList } from "@/types/types";
+import { MediaList, MediaListData } from "@/types/types";
 
 type MediaListItemProps = {
-  mediaItem: MediaList;
+  mediaItem: MediaListData;
 };
 
 export default function MediaListItem({ mediaItem }: MediaListItemProps) {
   return (
     <View>
-      <Text style={{ color: "white" }}>{mediaItem.title}</Text>
+      <Image
+        source={{ uri: mediaItem.image }}
+        style={{ width: 110, aspectRatio: 3 / 4 ,marginHorizontal:5,borderRadius:5}}
+      />
     </View>
   );
 }
