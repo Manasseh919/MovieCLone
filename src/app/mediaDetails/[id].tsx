9594c37b2d4669e728a2aaa479a2dc3081433843
Mann919-dusk <manasseh.ameyow@glodusk.com>
2025-06-03 19:50:53 +0200

    mediadetails update

--- src/app/mediaDetails/[id].tsx
+++ src/app/mediaDetails/[id].tsx
@@ -1,30 +1,37 @@
 import { useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import mediaDetailedList from "@assets/data/mediaDetailedList.json";
+import mediaDetailedList from '@assets/data/mediaDetailedList.json';
 import MediaInfo from "@/components/MediaDetails/MediaInfo";
 
 export default function MediaDetails() {
-  const { id } = useLocalSearchParams();
+  const { id } = useLocalSearchParams()
 
-  const mediaItem = mediaDetailedList.find((media) => media.id === id);
+  const mediaItem = mediaDetailedList.find((media) => media.id === id)
 
   if (!mediaItem) {
-    return <Text style={{ color: "white" }}>Media Not Found!</Text>;
+    return <Text>Media Not Found!</Text>;
   }
 
-  const { title, description, ageRestriction, duration, type, releaseYear } =
-    mediaItem;
+  const { type, title, description, releaseYear, ageRestriction, duration, thumbnail, trailer, videoUrl, seasons } = mediaItem;
+
+  const videoSource = type === 'MOVIE' ? videoUrl : seasons?.[0]?.episodes?.[0]?.videoUrl;
+
+  if (!videoSource) {
+    return <Text>No playable video found.</Text>;
+  }
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
       <MediaInfo
         title={title}
-        description={description}
+        releaseYear={releaseYear}
         ageRestriction={ageRestriction}
         duration={duration}
+        description={description}
         type={type}
-        releaseYear={releaseYear}
+        nrOfSeasons={seasons?.length}
       />
     </SafeAreaView>
-  );
+  )
 }
