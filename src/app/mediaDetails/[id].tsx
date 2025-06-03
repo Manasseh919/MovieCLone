62701bbc6d0802abc9a6a10793f82da24eb53cc7
Mann919-dusk <manasseh.ameyow@glodusk.com>
2025-06-03 11:10:11 +0200

    fetching media data

--- src/app/mediaDetails/[id].tsx
+++ src/app/mediaDetails/[id].tsx
@@ -1,9 +1,19 @@
+import { useLocalSearchParams } from "expo-router";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import mediaDetailedList from "@assets/data/mediaDetailedList.json";
 
 export default function MediaDetails() {
+    const {id}  = useLocalSearchParams()
+
+    const mediaItem = mediaDetailedList.find((media) => media.id === id)
+
+     if (!mediaItem) {
+    return <Text style={{color: 'white'}}>Media Not Found!</Text>;
+  }
     return (
-        <View>
-            <Text>Media Details</Text>
-        </View>
+        <SafeAreaView>
+            <Text style={{color: 'white'}}>{mediaItem.title}</Text>
+        </SafeAreaView>
     )
 }
